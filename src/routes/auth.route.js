0ab30e5c0941a3424b7a498e3c9ec03bbc8c637c
Nguyen Khoi <khoibene@gmail.com>
2025-09-01 09:56:37 +0700

    google

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,6 +3,7 @@ import authController from '../controllers/auth.controller.js'
 import asyncHandler from '../middlewares/asyncHandle.js'
 import { registerValidator, loginValidator } from '../middlewares/validators/auth.validator.js'
 import { handleValidation } from '../middlewares/validators/handleValidation.js'
+import passport from 'passport'
 
 const router = express.Router()
 
@@ -13,5 +14,7 @@ router.post('/logout', asyncHandler(authController.logout))
 router.post('/send-otp', asyncHandler(authController.sendOtp))
 router.post('/verify-otp', asyncHandler(authController.verifyOtp))
 router.post('/reset-password', asyncHandler(authController.resetPassword))
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login', session: false }), asyncHandler(authController.oauthCallback))
 
 export default router
